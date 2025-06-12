91fdfe06b9a1f01e571da9ea83ef77e815a31386
Ppantnt <tntppap1@gmail.com>
2025-06-13 00:33:16 +0800

    another 2h session ended

--- template/projectile.gd
+++ template/projectile.gd
@@ -1,4 +1,5 @@
 extends KinematicBody2D
+class_name Projectile
 
 var vel=Vector2(0,0)
 
@@ -6,6 +7,7 @@ func _ready():
 	rotation=vel.angle()+PI/2
 
 func _process(delta):
+	z_index=position.y
 	update_state(delta)
 	rotation=vel.angle()+PI/2
 	var collision=move_and_collide(vel*delta)
@@ -19,6 +21,8 @@ func update_state(delta):
 	pass
 
 func collide_action(collider):
+	collider.damage(1)
+	
 	queue_free()
 	#print(collider)
 	#pass
